c4a87d8360215eb9ce811094d176099ee1f12c23
raph <raphrien@gmail.com>
2020-10-21 17:15:49 -0400

    créer une plateform animée avec une trajectoire

---
+++ scripts/rectangle.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+var speed = 3000
+var velocity = Vector2()
+var target = Vector2()
+
+export(NodePath) var path
+var path_points
+var path_point_index = 0
+
+
+func _ready():
+	if path:
+		path_points = get_node(path).curve.get_baked_points()
+
+func _physics_process(delta):
+	if !path:
+		return
+	var target = path_points[path_point_index]
+	if position.distance_to(target) < 1:
+		path_point_index = wrapi(path_point_index+1, 0, path_points.size())
+		target = path_points[path_point_index]
+	
+	velocity = (target-position).normalized() * speed
+	velocity = move_and_slide(velocity, delta)
+	
+	
